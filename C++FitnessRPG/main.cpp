dd972c59ad0f0979d430d307c4a063312a41a775
Barbara Johnson <barbarajohnson@Barbaras-MacBook-Pro.local>
2017-06-07 14:54:07 -0500

    Testing process and added minor player and NPC data.

--- C++FitnessRPG/main.cpp
+++ C++FitnessRPG/main.cpp
@@ -8,8 +8,32 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Function practice
+int OpponentInfo()
+{
+    
+    int stamina = 15;
+    
+    std::cout << "You will be chased across the map by Self Doubt," << std::endl;
+    std::cout << "which is a vampire of incredible persistence and tenancity." << std::endl;
+    std::cout << "Self Doubt has stamina of: " << stamina << std::endl;
+    
+    
     return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int PlayerStamina = 0;
+    std::string PlayerName;
+    
+    std::cout << "What is your name? ";
+    std::cin >> PlayerName;
+    
+    std::cout << "Enter a digit between 1 and 15: ";
+    std::cin >> PlayerStamina;
+    
+    
+    
+    return OpponentInfo();
 }
