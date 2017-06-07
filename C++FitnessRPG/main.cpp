36930137cb4de008e129caf18d35eab1c2f2889b
Barbara Johnson <barbarajohnson@Barbaras-MacBook-Pro.local>
2017-06-07 09:45:02 -0500

    Initial Commit

---
+++ C++FitnessRPG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++FitnessRPG
+//
+//  Created by Barbara Johnson on 6/7/17.
+//  Copyright © 2017 Barbara Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
